6a67854b3b732e39708173e642805bab66325cf4
Simon Gladman <sgladman@gmail.com>
2016-04-28 10:29:50 +0100

    Initial Commit

---
+++ VideoEffects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoEffects
+//
+//  Created by Simon Gladman on 28/04/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
